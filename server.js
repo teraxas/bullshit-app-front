97e48c90dad98ec7fbf3febf9a41aa2dc197b3a5
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2017-09-10 19:09:43 +0300

    Redirect to https for heroku

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.use((req, res, next) => {
+    if (req.header['x-forwarded-proto'] !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+        next();
+    }
+  });
+
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
